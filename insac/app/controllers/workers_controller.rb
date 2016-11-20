8c9694b9cda1a8085e1c36c29bb80c1377c4c237
Alvar0ne <alvaro_utem@hotmail.com>
2016-11-20 14:24:00 -0300

    Agregado editar trabajador y eliminar

--- insac/app/controllers/workers_controller.rb
+++ insac/app/controllers/workers_controller.rb
@@ -36,14 +36,15 @@ end
 
 	def create
 	
-	@worker = Worker.new(nombre: params[:worker][:nombre], 
-		                   rut: params[:worker][:rut], 
-		                   direccion: params[:worker][:direccion], 
-		                   email: params[:worker][:email],
-		                   telefono: params[:worker][:telefono], 
-		                   equipo: params[:worker][:equipo],    
-		                   jefe: params[:worker][:jefe])
+	#@worker = Worker.new(nombre: params[:worker][:nombre], 
+	#	                   rut: params[:worker][:rut], 
+	#	                   direccion: params[:worker][:direccion], 
+	#	                   email: params[:worker][:email],
+	#	                   telefono: params[:worker][:telefono], 
+	#	                   equipo: params[:worker][:equipo],    
+	#	                   jefe: params[:worker][:jefe])
 
+     @worker = Worker.new(worker_params) 
 
 
 	if @worker.save
@@ -55,5 +56,40 @@ end
     end
 
     end
+
+
+   def destroy
+	#DELETE FROM Articles
+
+	@worker = Worker.find(params[:id])
+	@worker.destroy #destroy elimina el objeto de la bd
+	redirect_to workers_path
+end
+
+
+
+
+
+    
+    def update
+	#UPDATE
+    
+	# @article.update_attributes({title:{'Nuevo Titulo'})
+	@worker = Worker.find(params[:id])
+	if @worker.update(worker_params)
+        redirect_to @worker
+    else
+    	render :edit
+    end
+
+end
+
+	
+
+	private
+	def worker_params
+		params.require(:worker).permit(:nombre,:rut,:direccion,:email,:telefono,:equipo,:jefe)
+	end
+
 
 end
