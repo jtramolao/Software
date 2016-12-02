63515b3145af34d8d9425a94dea8b54350f3e92a
jtramolao <juan.agustin-@live.com>
2016-12-02 10:35:40 -0300

    base de datos en postgresql

--- insac/db/schema.rb
+++ insac/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161118133830) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "workers", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "rut"
